538f7a51fb3b4ab3c6b22ea617e03d0775fb1ba7
Duy <38127890+vietduy1512@users.noreply.github.com>
2019-09-14 20:35:55 +0700

    Implement full react tutorial

--- react-tictactoe/src/index.js
+++ react-tictactoe/src/index.js
@@ -54,12 +54,14 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       isXTurn: true,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    //const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -70,6 +72,7 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       isXTurn: !this.state.isXTurn,
     });
     // }, () => {
@@ -89,10 +92,29 @@ class Game extends React.Component {
   //   })
   // }
 
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      isXTurn: (step % 2) === 0,
+    });
+  }
+
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    //const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      )
+    })
 
     let status;
     if (winner) {
@@ -111,7 +133,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
