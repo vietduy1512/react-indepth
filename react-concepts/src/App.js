0b910bc15c8d2aacdb2fa9c3c214a93e93502f3d
Duy Le <Duy.LeViet1@nashtechglobal.com>
2019-12-02 18:57:28 +0700

    Update files structure

--- react-concepts/src/App.js
+++ react-concepts/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import HelloWorldComponent from './components/hello-world/hello-world';
-import IntroducingJSX from './components/introducing-JSX/introducing-JSX';
+import HelloWorldComponent from './components/hello-world/index';
+import IntroducingJSX from './components/introducing-JSX';
 
 function App() {
   return (
