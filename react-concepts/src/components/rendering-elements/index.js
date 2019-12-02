ce5ce571a9f84b9a5093eb158a3cda29539b82c7
Duy <vietduy1512@gmail.com>
2019-12-02 19:27:11 +0700

    Revert rendering elements to basic

--- react-concepts/src/components/rendering-elements/index.js
+++ react-concepts/src/components/rendering-elements/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
 function RenderingElements() {
-  // Code below updates ALL, which is not optimized as React
-  /*
-  document.getElementById('root').innerHTML = `
-    <span>Time: </span>
-    <span>${new Date().toLocaleTimeString()}</span>
-  `;
-  */
+  //// Code below updates ALL, which is not optimized as React
+  // document.getElementById('root').innerHTML = `
+  //   <span>Time: </span>
+  //   <span>${new Date().toLocaleTimeString()}</span>
+  // `;
+
   return (
     <div id="rendering-elements">
       <span>Time: </span>
