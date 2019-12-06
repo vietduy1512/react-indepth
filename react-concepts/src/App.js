a80795a5cbb6179555c8a97215b81bae6b2e0492
Duy <38127890+vietduy1512@users.noreply.github.com>
2019-12-06 23:05:58 +0700

    Implement navbar structure

--- react-concepts/src/App.js
+++ react-concepts/src/App.js
@@ -1,96 +1,30 @@
 import React from 'react';
-import HelloWorldComponent from './components/1.hello-world/index';
-import IntroducingJSX from './components/2.introducing-JSX';
-import RenderingElements from './components/3.rendering-elements';
-import ComponentsAndProps from './components/4.components-and-props';
-import StateAndLifecycle from './components/5.state-and-lifecycle';
-import HandlingEvents from './components/6.handling-events';
-import { UnableToUpdateComponents, AbleToUpdateComponents } from './components/examples/update-components';
+import components from './components'
 
 function App() {
   return (
     <div>
-      <div class="row">
-        <div class="col-3">
-          <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-            <a class="nav-link active" data-toggle="pill" role="tab"
-              href="#v-pills-hello-world"
-            >
-              1. Hello World
-            </a>
-            <a class="nav-link" data-toggle="pill" role="tab"
-              href="#v-pills-introducing-jsx"
-            >
-              2. Introducing JSX
-            </a>
-            <a class="nav-link" data-toggle="pill" role="tab" 
-              href="#v-pills-rendering-elements"
-            >
-              3. Rendering Elements
-            </a>
-            <a class="nav-link" data-toggle="pill" role="tab" 
-              href="#v-pills-components-and-props"
-            >
-              4. Components And Props
-            </a>
-            <a class="nav-link" data-toggle="pill" role="tab" 
-              href="#v-pills-state-and-lifecycle"
-            >
-              5. State And Lifecycle
-            </a>
-            <a class="nav-link" data-toggle="pill" role="tab" 
-              href="#v-pills-handling-events"
-            >
-              6. Handling Events
-            </a>
-            <a class="nav-link" data-toggle="pill" role="tab" 
-              href="#v-pills-examples"
-            >
-              Examples
-            </a>
+      <div className="row">
+        <div className="col-3">
+          <div className="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+            {components.map(component => (
+              <a className="nav-link" data-toggle="pill" role="tab"
+                href={'#' + component.id} key={component.id}
+              >
+                {component.title}
+              </a>
+            ))}
           </div>
         </div>
-        <div class="col-9">
-          <div class="tab-content" id="v-pills-tabContent">
-            <div class="tab-pane fade show active" role="tabpanel"
-              id="v-pills-hello-world"
-            >
-              <HelloWorldComponent />
-            </div>
-            <div class="tab-pane fade" role="tabpanel"
-              id="v-pills-introducing-jsx"
-            >
-              <IntroducingJSX />
-            </div>
-            <div class="tab-pane fade" role="tabpanel"
-              id="v-pills-rendering-elements"
-            >
-              <RenderingElements />
-            </div>
-            <div class="tab-pane fade" role="tabpanel"
-              id="v-pills-components-and-props"
-            >
-              <ComponentsAndProps />
-            </div>
-            <div class="tab-pane fade" role="tabpanel"
-              id="v-pills-state-and-lifecycle"
-            >
-              <StateAndLifecycle />
-            </div>
-            <div class="tab-pane fade" role="tabpanel"
-              id="v-pills-handling-events"
-            >
-              <HandlingEvents />
-            </div>
-            <div class="tab-pane fade" role="tabpanel"
-              id="v-pills-examples"
-            >
-              <div>
-                <h3>Examples</h3>
-                <UnableToUpdateComponents />
-                <AbleToUpdateComponents />
+        <div className="col-9">
+          <div className="tab-content" id="v-pills-tabContent">
+            {components.map(component => (
+              <div className="tab-pane fade" role="tabpanel"
+                id={component.id} key={component.id}
+              >
+                {component.content}
               </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
---
+++ react-concepts/src/components/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import HelloWorldComponent from './1.hello-world/index';
+import IntroducingJSX from './2.introducing-JSX';
+import RenderingElements from './3.rendering-elements';
+import ComponentsAndProps from './4.components-and-props';
+import StateAndLifecycle from './5.state-and-lifecycle';
+import HandlingEvents from './6.handling-events';
+import { UnableToUpdateComponents, AbleToUpdateComponents } from './examples/update-components';
+
+const components = [
+    {
+        title: "1. Hello World",
+        id: "v-pills-hello-world",
+        content: <HelloWorldComponent />
+    },
+    {
+        title: "2. Introducing JSX",
+        id: "v-pills-introducing-jsx",
+        content: <IntroducingJSX />
+    },
+    {
+        title: "3. Rendering Elements",
+        id: "v-pills-rendering-elements",
+        content: <RenderingElements />
+    },
+    {
+        title: "4. Components And Props",
+        id: "v-pills-components-and-props",
+        content: <ComponentsAndProps />
+    },
+    {
+        title: "5. State And Lifecycle",
+        id: "v-pills-state-and-lifecycle",
+        content: <StateAndLifecycle />
+    },
+    {
+        title: "6. Handling Events",
+        id: "v-pills-handling-events",
+        content: <HandlingEvents />
+    },
+    {
+        title: "Examples",
+        id: "v-pills-examples",
+        content: (
+        <div>
+            <h3>Examples</h3>
+            <UnableToUpdateComponents />
+            <AbleToUpdateComponents />
+        </div>
+        )
+    }
+];
+
+export default components;
