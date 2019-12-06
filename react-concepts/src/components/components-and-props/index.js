fa5f281087514858bb3c1934b09bb05994723553
Duy <vietduy1512@gmail.com>
2019-12-06 14:23:43 +0700

    Implement "Components and Props" section

--- react-concepts/src/components/components-and-props/index.js
+++ react-concepts/src/components/components-and-props/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import FunctionComponent from './function-component'
 import ClassComponent from './class-component'
 
+// 1. Composing Components:     Components can refer to other components in their output (like calling function and class component below)
+// 2. Extracting Components:    Don’t be afraid to split components into smaller components.
+// 3. Props are Read-Only:      Never modify its own props/input parameters (follow functional programming principles)
+
 function ComponentsAndProps() {
   return (
     <div>
       <h2>Components And Props</h2>
-      <FunctionComponent name="Duy" />  {/* Composing Components: is calling other components */}
+      <FunctionComponent name="Duy" />  {/*  */}
       <ClassComponent name="Duy" />
     </div>
   );
