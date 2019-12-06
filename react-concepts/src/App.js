48d2776f3e715d96427a96c68ad1e1305fee1b27
Duy <vietduy1512@gmail.com>
2019-12-06 16:03:54 +0700

    Update folder names

--- react-concepts/src/App.js
+++ react-concepts/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import HelloWorldComponent from './components/hello-world/index';
-import IntroducingJSX from './components/introducing-JSX';
-import RenderingElements from './components/rendering-elements';
-import ComponentsAndProps from './components/components-and-props';
-import StateAndLifecycle from './components/state-and-lifecycle';
+import HelloWorldComponent from './components/1.hello-world/index';
+import IntroducingJSX from './components/2.introducing-JSX';
+import RenderingElements from './components/3.rendering-elements';
+import ComponentsAndProps from './components/4.components-and-props';
+import StateAndLifecycle from './components/5.state-and-lifecycle';
 import { UnableToUpdateComponents, AbleToUpdateComponents } from './components/examples/update-components';
 
 function App() {
---
+++ react-concepts/src/components/1.hello-world/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HelloWorld() {
+  return (
+    <div>
+      <h2>Hello World</h2>
+    </div>
+  );
+}
+
+export default HelloWorld;
