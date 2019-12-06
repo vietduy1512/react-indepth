1a6a41e708600e1003b00d5d8f73b6d58bd14419
Duy <vietduy1512@gmail.com>
2019-12-06 21:11:02 +0700

    Implement navbar

--- react-concepts/src/App.js
+++ react-concepts/src/App.js
@@ -10,17 +10,89 @@ import { UnableToUpdateComponents, AbleToUpdateComponents } from './components/e
 function App() {
   return (
     <div>
-      <HelloWorldComponent />
-      <IntroducingJSX />
-      <RenderingElements />
-      <ComponentsAndProps />
-      <StateAndLifecycle />
-      <HandlingEvents />
-
-      <div>
-        <h3>Examples</h3>
-        <UnableToUpdateComponents />
-        <AbleToUpdateComponents />
+      <div class="row">
+        <div class="col-3">
+          <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+            <a class="nav-link active" data-toggle="pill" role="tab"
+              href="#v-pills-hello-world"
+            >
+              1. Hello World
+            </a>
+            <a class="nav-link" data-toggle="pill" role="tab"
+              href="#v-pills-introducing-jsx"
+            >
+              2. Introducing JSX
+            </a>
+            <a class="nav-link" data-toggle="pill" role="tab" 
+              href="#v-pills-rendering-elements"
+            >
+              3. Rendering Elements
+            </a>
+            <a class="nav-link" data-toggle="pill" role="tab" 
+              href="#v-pills-components-and-props"
+            >
+              4. Components And Props
+            </a>
+            <a class="nav-link" data-toggle="pill" role="tab" 
+              href="#v-pills-state-and-lifecycle"
+            >
+              5. State And Lifecycle
+            </a>
+            <a class="nav-link" data-toggle="pill" role="tab" 
+              href="#v-pills-handling-events"
+            >
+              6. Handling Events
+            </a>
+            <a class="nav-link" data-toggle="pill" role="tab" 
+              href="#v-pills-examples"
+            >
+              Examples
+            </a>
+          </div>
+        </div>
+        <div class="col-9">
+          <div class="tab-content" id="v-pills-tabContent">
+            <div class="tab-pane fade show active" role="tabpanel"
+              id="v-pills-hello-world"
+            >
+              <HelloWorldComponent />
+            </div>
+            <div class="tab-pane fade" role="tabpanel"
+              id="v-pills-introducing-jsx"
+            >
+              <IntroducingJSX />
+            </div>
+            <div class="tab-pane fade" role="tabpanel"
+              id="v-pills-rendering-elements"
+            >
+              <RenderingElements />
+            </div>
+            <div class="tab-pane fade" role="tabpanel"
+              id="v-pills-components-and-props"
+            >
+              <ComponentsAndProps />
+            </div>
+            <div class="tab-pane fade" role="tabpanel"
+              id="v-pills-state-and-lifecycle"
+            >
+              <StateAndLifecycle />
+            </div>
+            <div class="tab-pane fade" role="tabpanel"
+              id="v-pills-handling-events"
+            >
+              <HandlingEvents />
+            </div>
+            <div class="tab-pane fade" role="tabpanel"
+              id="v-pills-examples"
+            >
+              <div>
+                <h3>Examples</h3>
+                <UnableToUpdateComponents />
+                <AbleToUpdateComponents />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
