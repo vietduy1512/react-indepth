8439bcc8d335802865b17c130f7fdaf2603695a0
Duy <38127890+vietduy1512@users.noreply.github.com>
2019-12-08 15:31:59 +0700

    Implement "Forms" section

--- react-concepts/src/components/9.forms/index.js
+++ react-concepts/src/components/9.forms/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 
-class NameForm extends React.Component {
+// Extension: https://jaredpalmer.com/formik
+
+class BasicForm extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
@@ -72,10 +74,74 @@ class NameForm extends React.Component {
     }
 }
 
+
+class HandleMultipleInputForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        name: '',
+        essay: '',
+        flavor: ''
+      };
+
+      this.handleInputChange = this.handleInputChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(event) {
+        const value = event.target.value;
+        const name = event.target.name;
+        this.setState({[name]: value});     // Using ES6
+      }
+
+    handleSubmit(event) {
+      alert(`A form was submitted \nName:${this.state.name} - Essay:${this.state.essay} - Flavor:${this.state.flavor}`);
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name: 
+            <input type="text" name="name" value={this.state.name} onChange={this.handleInputChange} />
+          </label>
+          <br/>
+
+          <label>
+            Essay:
+            <textarea name="essay" value={this.state.essay} onChange={this.handleInputChange} />
+          </label>
+          <br/>
+
+          <label>
+            Pick your favorite flavor:
+            <select name="flavor" value={this.state.flavor} onChange={this.handleInputChange}>
+              <option value="grapefruit">Grapefruit</option>
+              <option value="lime">Lime</option>
+              <option value="coconut">Coconut</option>
+              <option value="mango">Mango</option>
+            </select>
+          </label>
+          <br/>
+
+          <input type="file" />
+          <br/>
+
+          <input type="submit" value="Submit" />
+        </form>
+      );
+    }
+}
+
 function Form() {
     return (
         <>
-            <NameForm />
+            <h4>Basic form</h4>
+            <BasicForm />
+            <hr/>
+            <h4>Handle multiple input</h4>
+            <HandleMultipleInputForm />
         </>
     )
 }
