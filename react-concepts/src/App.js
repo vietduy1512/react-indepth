abe5be5e2218a31b28448abe7314b3321272e532
Duy <vietduy1512@gmail.com>
2019-12-11 14:39:10 +0700

    Implement basic router

--- react-concepts/src/App.js
+++ react-concepts/src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import MainConcepts from './components/main-concepts'
+import {
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <MainConcepts />
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/main-concepts">Main Concepts</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route exact path="/">
+          <div>Home page</div>
+        </Route>
+        <Route path="/main-concepts">
+          <MainConcepts />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- react-concepts/src/index.js
+++ react-concepts/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from "react-router-dom";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 
 //// For "Rendering Elements"
 // const main = () => {
