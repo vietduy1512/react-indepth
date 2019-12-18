cfd59bf1f43b0dc742f63a7ffd1f76b9dc3e5f3e
Duy <vietduy1512@gmail.com>
2019-12-18 23:03:16 +0700

    Add redux packages and Implement Provider

--- redux-concepts/src/App.js
+++ redux-concepts/src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./App.css";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
 import Posts from "./components/Posts";
 import PostForm from "./components/Postform";
 
+const store = createStore(()=> [], {}, applyMiddleware());
+
 function App() {
   return (
-    <div>
+    <Provider store={store}>
       <PostForm></PostForm>
       <hr />
       <Posts></Posts>
-    </div>
+    </Provider>
   );
 }
 
