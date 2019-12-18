96d09db8232724122b35c00adf5294358d7f2fc4
Duy <vietduy1512@gmail.com>
2019-12-19 01:05:08 +0700

    Implement add new post to existing posts list with redux

--- redux-concepts/src/components/Posts.js
+++ redux-concepts/src/components/Posts.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions/postAction";
 
 class Posts extends Component {
   componentWillMount() {
     this.props.fetchPosts();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.newPost) {
+      this.props.posts.unshift(nextProps.newPost);
+    }
   }
 
   render() {
@@ -26,11 +32,13 @@ class Posts extends Component {
 
 Posts.propTypes = {
   fetchPosts: PropTypes.func.isRequired,
-  posts: PropTypes.array.isRequired
-}
+  posts: PropTypes.array.isRequired,
+  newPost: PropTypes.object
+};
 
 const mapStateToProps = state => ({
-  posts: state.posts.items
-})
+  posts: state.posts.items,
+  newPost: state.posts.item
+});
 
 export default connect(mapStateToProps, { fetchPosts })(Posts);
