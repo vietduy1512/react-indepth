1926bcfdbcd8db22f782e8b46e369b60f693927e
Duy <vietduy1512@gmail.com>
2019-12-19 23:42:57 +0700

    Remove redundant comments

--- redux-thunk-post-list/src/actions/postAction.js
+++ redux-thunk-post-list/src/actions/postAction.js
@@ -17,31 +17,4 @@ export const createPost = post => dispatch => {
     payload: post
   });
 
-  //// Example of calling API to create post
-  // fetch("https://jsonplaceholder.typicode.com/posts", {
-  //   method: "POST",
-  //   headers: {
-  //     "content-type": "application/json"
-  //   },
-  //   body: JSON.stringify(post)
-  // })
-  //   .then(res => res.json())
-  //   .then(postData =>
-  //     dispatch({
-  //       type: NEW_POST,
-  //       payload: postData
-  //     })
-  //   );
 };
-
-//// Basic way to implement fetchPosts action
-// export function fetchPosts() {
-//   return function(dispatch) {
-//     fetch("https://jsonplaceholder.typicode.com/posts")
-//       .then(res => res.json())
-//       .then(posts => dispatch({
-//         type: FETCH_POSTS,
-//         payload: posts
-//       }));
-//   };
-// }
