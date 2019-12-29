5dbe866c07dc0cccb0c37a982fd19e4431a57108
Duy <38127890+vietduy1512@users.noreply.github.com>
2019-12-29 19:32:32 +0700

    Implement "Adding CSS Modules" section

--- react-concepts/src/components/styles-and-assets/3.adding-css-modules/index.js
+++ react-concepts/src/components/styles-and-assets/3.adding-css-modules/index.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import styles from './mymodule.module.css';   // Import css modules stylesheet as styles (index.module.css not working)
-//import './another-stylesheet.css';          // Import regular stylesheet
+import './another-stylesheet.css';          // Import regular stylesheet
 
 class AddingCSSModules extends Component {
   render() {
-    console.log(styles['colortext']);
     return <h3 className={styles.colortext}>Color Text</h3>;
   }
 }
