e2924449e9b50402e3d8de204e6afd2515477804
Duy <38127890+vietduy1512@users.noreply.github.com>
2019-12-29 16:14:06 +0700

    Implement "Adding Stylesheets" section

---
+++ react-concepts/src/components/styles-and-assets/1.adding-stylesheets/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class ColorHeading extends Component {
+  render() {
+    return <h3 className="color-text">Heading</h3>;
+  }
+}
+
+export default ColorHeading;
