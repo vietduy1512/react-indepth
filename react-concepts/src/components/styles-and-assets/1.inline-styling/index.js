c4bfbb04e5f30554ca955f41a341426566e16540
Duy <38127890+vietduy1512@users.noreply.github.com>
2019-12-29 18:46:45 +0700

    Implement inline styling

---
+++ react-concepts/src/components/styles-and-assets/1.inline-styling/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class InlineStyling extends Component {
+  render() {
+    const mystyle = {
+      color: "white",
+      backgroundColor: "DodgerBlue",
+      padding: "10px",
+      fontFamily: "Arial"
+    };
+
+    return (
+      <>
+        <h3 style={{color: "red"}}>Basic</h3>
+        <h3 style={{backgroundColor: "lightblue"}}>camelCased Property Names</h3> {/* in css: background-color */}
+        <h3 style={{mystyle}}>JavaScript Object</h3>
+      </>
+    );
+  }
+}
+
+export default InlineStyling;
--- react-concepts/src/components/styles-and-assets/components.js
+++ react-concepts/src/components/styles-and-assets/components.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import ColorHeading from './1.adding-stylesheets';
+import InlineStyling from './1.inline-styling';
+import AddingStylesheets from './2.adding-stylesheets';
 
 const components = [
     {
         isSelected: true,
+        title: "0. Inline Styling",
+        id: "v-pills-inline-styling",
+        content: <InlineStyling/>
+    },
+    {
         title: "1. Adding Stylesheets",
         id: "v-pills-adding-stylesheets",
-        content: <ColorHeading/>
+        content: <AddingStylesheets/>
     }
 ];
 
